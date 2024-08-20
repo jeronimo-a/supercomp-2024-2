6b1c0f8b7a9a82ed7e05511627e70f07e1c603f5
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-08-20 14:02:23 -0300

    Atividade 3.2, item 4: métodos de redimensionamento e query feitos

--- aulas/03-linguagem-cpp/atividade-2/item-4/item-4.cpp
+++ aulas/03-linguagem-cpp/atividade-2/item-4/item-4.cpp
@@ -3,7 +3,6 @@
 
 class Vector {
     public:
-        int get(int index) const;
         void set(int index, int valor);
         void inserir(int index, int valor);
         void remover(int index);
@@ -14,6 +13,10 @@ class Vector {
 
         void inicializa(int valor) {
             dados = std::vector<int>(capacidade, valor);
+        }
+
+        int get(int index) const {
+            return dados[index];
         }
 
         void imprime() const {
@@ -27,15 +30,19 @@ class Vector {
         std::vector<int> dados;
         int tam;
         int capacidade;
-        void redimensiona(int novaCapacidade);
+        void redimensiona(int novaCapacidade) {
+            capacidade = novaCapacidade;
+            dados.resize(capacidade);
+        }
 };
 
 // Implementação das funções...
 
 int main() {
     Vector vec(5);
-    vec.inicializa(0);
+    vec.inicializa(3);
     vec.imprime();
+    std::cout << vec.get(2) << std::endl;
 
     return 0;
 }
