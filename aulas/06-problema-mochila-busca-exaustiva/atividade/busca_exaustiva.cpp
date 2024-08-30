ca1596aa4ed703d92e9103bf61f7685b7c7f3e58
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-08-30 14:09:52 -0300

    Atividade 06: leitura dos arquivos de entrada feita

---
+++ aulas/06-problema-mochila-busca-exaustiva/atividade/busca_exaustiva.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    // faz a leitura do tamanho da quantidade de itens e capacidade da mochila
+    int quantidade_itens, capacidade_mochila;
+    std::cin >> quantidade_itens >> capacidade_mochila;
+
+    // define a matriz que contém todos os itens, itens[n] = [id, peso, valor]
+    std::vector<std::vector<int>> itens(quantidade_itens, std::vector<int>(3));
+
+    // faz a leitura dos valores e pesos de cada item
+    for (int i = 0; i < quantidade_itens; i++) {
+        itens[i][0] = i;
+        std::cin >> itens[i][1] >> itens[i][2];
+    }
+
+    return 0;
+}
